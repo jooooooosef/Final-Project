6a78162e45461697f3f6084225ef33ca6b579570
jooooooosef <106436709+jooooooosef@users.noreply.github.com>
2023-04-25 10:16:00 +0800

    test

--- [J] ver2/GameFrame.java
+++ [J] ver2/GameFrame.java
@@ -6,21 +6,26 @@ public class GameFrame {
     
     private JFrame frame;
     private GameCanvas gameCanvas;
-    private JPanel cp;
+    // private 
+    // private JPanel cp;
     
     public GameFrame(){
         frame = new JFrame();
-        cp = (JPanel) frame.getContentPane();
+        // cp = (JPanel) frame.getContentPane();
         gameCanvas = new GameCanvas();
     }
 
     public void setUpGUI(){
         frame.setTitle("Good day, Ateneans!");
     
-        cp.add(gameCanvas);
+        // cp.add(gameCanvas);
     
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void changeCanvas(){
+        // cp.re
     }
 }
